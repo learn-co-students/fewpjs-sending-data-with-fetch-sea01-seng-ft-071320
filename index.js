db002caa4bdc131f9446e9c2bb3ef77a284da318
afteralec <github email address>
2020-08-26 12:15:36 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(userName, userEmail) {
+  const submitBody = {
+    name: userName,
+    email: userEmail,
+  };
+
+  const submitConfig = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(submitBody),
+  };
+
+  return fetch("http://localhost:3000/users", submitConfig)
+    .then((resp) => resp.json())
+    .then((json) => appendToBody(json.id))
+    .catch((error) => {
+      appendToBody(error);
+    });
+}
+
+function appendToBody(string) {
+  document.querySelector("body").append(string);
+}
